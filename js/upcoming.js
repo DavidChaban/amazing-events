2b4c6ec034db9e0bb2bb322fe3c89a1d3bc55529
David Chaban <davidchaban01@gmail.com>
2022-08-09 03:11:24 -0300

    Upcoming.Js

--- js/upcoming.js
+++ js/upcoming.js
@@ -1,14 +1,20 @@
-let fechaActual = (data.currentDate);
-let text = "";
-array = data.events;
-let container1 = document.getElementById("container");
-let card = "";
-function displayCards(array) {
-  if(array.length > 0){
-    let card = ""
-    array.forEach(array =>{
-      if(fechaActual < array.date)
-    card += `<div class="card01 card">
+const api = 'https://amazing-events.herokuapp.com/api/events'
+fetch('https://amazing-events.herokuapp.com/api/events')
+  .then(Response => Response.json())
+  .then(data => variable(data))
+let variable = data => {
+  let fechaActual = data.currentDate
+  let text = ''
+  array = data.events
+  let container1 = document.getElementById('container')
+  let card = ''
+
+  function displayCards (array) {
+    if (array.length > 0) {
+      let card = ''
+      array.forEach(array => {
+        if (fechaActual < array.date)
+          card += `<div class="card01 card">
         <img src="${array.image}" class="card-img-top w-3 img2 imgCard"  alt="${array.name}">
         <div class="card-body d-flex flex-column justify-content-between">
         <div class="d-flex justify-content-start bg-black d-flex align-items-center justify-content-center align-content-center dateCard">
@@ -23,90 +29,90 @@ function displayCards(array) {
                    </div>
                    </div>
                    `
-    })       
-                  container1.innerHTML=card;
-}
-else{
-  container1.innerHTML = `<p style="color:black">No Search Found </p>`
-}
-}
-//-------generador de checks --------
-let checkboxContainer = document.getElementById("checkId");
-let checkboxs = [];
-let datahola = data.events.forEach((a) => {
-  if (!checkboxs.includes(a.category)) {
-    checkboxs.push(a.category);
+      })
+      container1.innerHTML = card
+    } else {
+      container1.innerHTML = `<p style="color:black">No Search Found </p>`
+    }
   }
-});
-//------imprimir------
-function printCategory() {
-  let checkboxs1 = "";
-  checkboxs.forEach((a) => {
-    // printCategory(a)
-    checkboxs1 += `<div class="form-check form-switch m-3">
+  //-------generador de checks --------
+  let checkboxContainer = document.getElementById('checkId')
+  let checkboxs = []
+  let datahola = data.events.forEach(a => {
+    if (!checkboxs.includes(a.category)) {
+      checkboxs.push(a.category)
+    }
+  })
+  //------imprimir------
+  function printCategory () {
+    let checkboxs1 = ''
+    checkboxs.forEach(a => {
+      // printCategory(a)
+      checkboxs1 += `<div class="form-check form-switch m-3">
   <input class="form-check-input" type="checkbox" role="switch" id="${a}">
   <label class="form-check-label" for="${a}">${a}</label>
-</div>`;
-    checkboxContainer.innerHTML = checkboxs1;
-  });
-}
-printCategory();
-//////*********************busqueda */
-let textVacie = "";
-function search() {
-  let search = document.getElementById("searchh");
-  search.addEventListener("keyup", (e) => {
-    textVacie = e.target.value;
-    filter();
-  });
-}
-search();
-///-----------Eventos click - checkbox-----------------
-let checkboxClick = [];
-let checkbox = document.querySelectorAll("input[type=checkbox]");
-function checkboxx() {
-  checkbox.forEach((checkedd) =>
-    checkedd.addEventListener("click", (event) => {
-      let checked = event.target.id;
-        if (checkedd.checked) {
-        checkboxClick.push(checked);
-        filter();
-      } else {
-        checkboxClick = checkboxClick.filter((uncheck) => uncheck !== checked);
-      
-      }
-      filter();
+</div>`
+      checkboxContainer.innerHTML = checkboxs1
     })
-  );
-}
-checkboxx();
-///----------- Filter()-----------------
-function filter() {
-  let filterArray = [];
-  if (checkboxClick.length > 0 && textVacie !== "") {
-      checkboxClick.map((checked) =>
-      filterArray.push(
-        ...array.filter(
-          (event) =>
-            event.name.toLowerCase().includes(textVacie.trim().toLowerCase()) &&
-            event.category == checked
+  }
+  printCategory()
+  //////*********************busqueda */
+  let textVacie = ''
+  function search () {
+    let search = document.getElementById('searchh')
+    search.addEventListener('keyup', e => {
+      textVacie = e.target.value
+      filter()
+    })
+  }
+  search()
+  ///-----------Eventos click - checkbox-----------------
+  let checkboxClick = []
+  let checkbox = document.querySelectorAll('input[type=checkbox]')
+  function checkboxx () {
+    checkbox.forEach(checkedd =>
+      checkedd.addEventListener('click', event => {
+        let checked = event.target.id
+        if (checkedd.checked) {
+          checkboxClick.push(checked)
+          filter()
+        } else {
+          checkboxClick = checkboxClick.filter(uncheck => uncheck !== checked)
+        }
+        filter()
+      })
+    )
+  }
+  checkboxx()
+  ///----------- Filter()-----------------
+  function filter () {
+    let filterArray = []
+    if (checkboxClick.length > 0 && textVacie !== '') {
+      checkboxClick.map(checked =>
+        filterArray.push(
+          ...array.filter(
+            event =>
+              event.name
+                .toLowerCase()
+                .includes(textVacie.trim().toLowerCase()) &&
+              event.category == checked
+          )
         )
       )
-    );
-  } else if (checkboxClick.length > 0 && textVacie === "") {
-      checkboxClick.map((checked) => {
-      filterArray.push(...array.filter((events) => events.category == checked));
-    }); 
-  } else if (checkboxClick.length == 0 && textVacie !== "") {
-    filterArray.push(
-      ...array.filter((event) =>
-        event.name.toLowerCase().includes(textVacie.trim().toLowerCase())
+    } else if (checkboxClick.length > 0 && textVacie === '') {
+      checkboxClick.map(checked => {
+        filterArray.push(...array.filter(events => events.category == checked))
+      })
+    } else if (checkboxClick.length == 0 && textVacie !== '') {
+      filterArray.push(
+        ...array.filter(event =>
+          event.name.toLowerCase().includes(textVacie.trim().toLowerCase())
+        )
       )
-    );
-  } else {
-       filterArray.push(...array);
+    } else {
+      filterArray.push(...array)
+    }
+    displayCards(filterArray)
   }
-  displayCards(filterArray);
+  filter()
 }
-filter();
-
